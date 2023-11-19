483d26533d567366314f811b311b73d6e72a4081
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-11-19 22:39:32 +0330

    Add some extra code about basics of JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Comment >>>>>>>>>
 // // One line comment
 // /* multi 
 // line
@@ -5,7 +6,7 @@
 
 //=============================================================
 
-// // var, let, const
+// // Variable (var, let, const) >>>>>>>>>
 // const myName = 'Ali';
 // const age = 10;
 // const retain = 4.5;
@@ -17,14 +18,14 @@
 
 //=============================================================
 
-// //Concatenation
+// //Concatenation >>>>>>>>>
 // console.log('My name is ' + myName + ' and I am ' + age);
 // //Template String
 // console.log(`My name is ${myName} and I am ${age}`);
 
 //=============================================================
 
-// // String Methods
+// // String Methods >>>>>>>>>
 // const s = 'Hello World!';
 // console.log(s.length)
 // console.log(s.toUpperCase())
@@ -34,7 +35,7 @@
 
 //=============================================================
 
-// //Array - Variables that hold multiple values
+// //Array - Variables that hold multiple values >>>>>>>>>
 // const numbers = new Array(1,2,3,4,5);
 // const fruits = ['Apple','Orange','Banana', true, 10];
 // console.log(numbers);
@@ -52,7 +53,7 @@
 
 //=============================================================
 
-// //Object Literals
+// //Object Literals >>>>>>>>>
 // const person = {
 //     firstName: 'Ali',
 //     lastName: 'Darvishi',
@@ -72,23 +73,23 @@
 // console.log(city);
 // person.email = 'Ali@gmail.com';
 // console.log(person)
-const todos = [
-    {
-        id:1,
-        text:'Take out trash',
-        isCompleted:true,
-    },
-    {
-        id:2,
-        text:'Meeting with boss',
-        isCompleted:true,
-    },
-    {
-        id:3,
-        text:'Dentist appointment',
-        isCompleted:false,
-    },
-]
+// const todos = [
+//     {
+//         id:1,
+//         text:'Take out trash',
+//         isCompleted:true,
+//     },
+//     {
+//         id:2,
+//         text:'Meeting with boss',
+//         isCompleted:true,
+//     },
+//     {
+//         id:3,
+//         text:'Dentist appointment',
+//         isCompleted:false,
+//     },
+// ]
 // console.log(todos);
 // console.log(todos[1].text);
 // const todosJSON = JSON.stringify(todos);
@@ -96,7 +97,7 @@ const todos = [
 
 //=============================================================
 
-// //For
+// //For >>>>>>>>>
 // for(let i=0; i<10; i++){
 //     console.log(`For loop Number: ${i}`)
 // }
@@ -111,7 +112,7 @@ const todos = [
 
 //=============================================================
 
-// //While
+// //While >>>>>>>>>
 // let i = 0;
 // while(i<11){
 //     console.log(`While Loop Number: ${i}`)
@@ -121,8 +122,7 @@ const todos = [
 
 //=============================================================
 
-//forEach, map, filter
-
+//forEach, map, filter >>>>>>>>>
 // todos.forEach(function(todo){
 //     console.log(todo.text)
 // });
@@ -130,19 +130,90 @@ const todos = [
 // const todoText = todos.map(function(todo){
 //     return todo.text
 // });
-
 // console.log(todoText);
 
 // const todoCompleted = todos.filter(function(todo){
 //     return todo.isCompleted === true;
 // });
-
 // console.log(todoCompleted);
 
-const todoCompleted = todos.filter(function(todo){
-    return todo.isCompleted === true;
-}).map(function(todo){
-    return todo.text
-})
+// const todoCompleted = todos.filter(function(todo){
+//     return todo.isCompleted === true;
+// }).map(function(todo){
+//     return todo.text
+// });
+// console.log(todoCompleted);
 
-console.log(todoCompleted);
+//=============================================================
+
+//IF >>>>>>>>>
+// const x = '10';
+// if (x == 10){
+//     console.log(`X is ${x}`);
+// };
+
+// const x = '20';
+// if (x === 10){
+//     console.log(`X is ${x}`);
+// } else if (x > 10){
+//     console.log('X is greater than 10');
+// }
+// else{
+//     console.log(`X is not ${x}`);
+// };
+
+// const x = 4;
+// const y = 9;
+// if (x > 5 || y < 10){ // && for and
+//     console.log('X is greater than 5 or Y is less than 10');
+// };
+
+// if(x>2){
+//     if(y< 10){
+//         console.log('Ok');
+//     }
+// };
+
+//=============================================================
+
+// //Ternary Operator >>>>>>>>>
+// const x = 10;
+// const color = x > 10 ? 'Red' : 'Blue';
+// console.log(color);
+
+//=============================================================
+
+// //Switch >>>>>>>>>
+// const x = 10;
+// const color = x > 10 ? 'Red' : 'Blue';
+// switch(color){
+//     case 'Red':
+//         console.log('Color Is Red');
+//         break;
+//     case 'Blue':
+//         console.log('Color Is Blue');
+//         break;
+//     default:
+//         console.log('Color Is not Red or Blue');
+//         break;
+// };
+
+//=============================================================
+
+//Function >>>>>>>>>
+// function addNums(num1, num2){
+//     console.log(num1+num2);
+// };
+// addNums(10,20)
+
+// function addNums(num1=1, num2=1){
+//     return num1+num2;
+// };
+// console.log(addNums(10,20));
+
+// const addNums = (num1=1, num2=1) => num1+num2; // {return num1+num2}
+// console.log(addNums(10,20));
+
+//=============================================================
+
+//Object Oriented Programming >>>>>>>>>

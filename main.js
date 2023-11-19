740d18fd4d7d42aeb4fde2a2446579178dac2332
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-11-19 21:50:27 +0330

    Add some basic javaScript code

---
+++ main.js
@@ -0,0 +1,148 @@
+// // One line comment
+// /* multi 
+// line
+// comment */
+
+//=============================================================
+
+// // var, let, const
+// const myName = 'Ali';
+// const age = 10;
+// const retain = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+// console.log(typeof z);
+
+//=============================================================
+
+// //Concatenation
+// console.log('My name is ' + myName + ' and I am ' + age);
+// //Template String
+// console.log(`My name is ${myName} and I am ${age}`);
+
+//=============================================================
+
+// // String Methods
+// const s = 'Hello World!';
+// console.log(s.length)
+// console.log(s.toUpperCase())
+// console.log(s.toLowerCase())
+// console.log(s.substring(0, 5))
+// console.log(s.split(' '))
+
+//=============================================================
+
+// //Array - Variables that hold multiple values
+// const numbers = new Array(1,2,3,4,5);
+// const fruits = ['Apple','Orange','Banana', true, 10];
+// console.log(numbers);
+// console.log(fruits);
+// console.log(fruits[0]);
+// fruits[3] = 'Pear';
+// console.log(fruits);
+// fruits.push('Grape');
+// console.log(fruits);
+// fruits.unshift('Lemon');
+// console.log(fruits);
+// fruits.pop();
+// console.log(Array.isArray(fruits))
+// console.log(fruits.indexOf('Orange'))
+
+//=============================================================
+
+// //Object Literals
+// const person = {
+//     firstName: 'Ali',
+//     lastName: 'Darvishi',
+//     age: 38,
+//     hobbies: ['music', 'movies', 'sports'],
+//     address: {
+//         street: 'Takhti st',
+//         city: 'Rasht',
+//         province: 'Guilan',
+//     }
+// };
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person.address.city);
+// const { firstName, lastName, address: { city } } = person;
+// console.log(lastName);
+// console.log(city);
+// person.email = 'Ali@gmail.com';
+// console.log(person)
+const todos = [
+    {
+        id:1,
+        text:'Take out trash',
+        isCompleted:true,
+    },
+    {
+        id:2,
+        text:'Meeting with boss',
+        isCompleted:true,
+    },
+    {
+        id:3,
+        text:'Dentist appointment',
+        isCompleted:false,
+    },
+]
+// console.log(todos);
+// console.log(todos[1].text);
+// const todosJSON = JSON.stringify(todos);
+// console.log(todosJSON);
+
+//=============================================================
+
+// //For
+// for(let i=0; i<10; i++){
+//     console.log(`For loop Number: ${i}`)
+// }
+
+// for(let i=0; i<todos.length; i++){
+//     console.log(todos[i].text)
+// }
+
+// for(let todo of todos){
+//     console.log(todo.text)
+// }
+
+//=============================================================
+
+// //While
+// let i = 0;
+// while(i<11){
+//     console.log(`While Loop Number: ${i}`)
+//     i++;
+// }
+
+
+//=============================================================
+
+//forEach, map, filter
+
+// todos.forEach(function(todo){
+//     console.log(todo.text)
+// });
+
+// const todoText = todos.map(function(todo){
+//     return todo.text
+// });
+
+// console.log(todoText);
+
+// const todoCompleted = todos.filter(function(todo){
+//     return todo.isCompleted === true;
+// });
+
+// console.log(todoCompleted);
+
+const todoCompleted = todos.filter(function(todo){
+    return todo.isCompleted === true;
+}).map(function(todo){
+    return todo.text
+})
+
+console.log(todoCompleted);
